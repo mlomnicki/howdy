3615f8dc13d6aa8a2fdb7a80aa095ae0135c4d53
Michał Łomnicki <michal.lomnicki@gmail.com>
2011-01-20 21:18:03 +0100

    got rid of jeweler in favor of bundler

--- lib/howdy.rb
+++ lib/howdy.rb
@@ -2,6 +2,7 @@ $:.unshift File.join(File.dirname(__FILE__), 'howdy')
 
 require 'core_ext/ruby/string/underscore'
 require 'core_ext/ruby/string/integer'
+require 'version'
 require 'config'
 require 'base'
 require 'ui'
